f4b36472619dd681cbbc247e91acd4e1756925c4
Raphaël Valyi <rvalyi@gmail.com>
2014-05-10 16:26:55 -0300

    code layout change to inherit from the Chef SCM resource and the Chef generic Provider

---
+++ resources/bzr.rb
@@ -0,0 +1,68 @@
+require "chef/resource/scm"
+
+class ::Chef
+  class Resource
+    class AkBzrBranch < ::Chef::Resource::Scm
+
+      def initialize(name, run_context=nil)
+        super
+        @resource_name = :ak_bzr_branch
+        @provider = ::Chef::Provider::AkBzr
+      end
+
+      def tarball(arg=nil)
+        set_or_return(
+          :tarball,
+          arg,
+          :kind_of => [String, FalseClass]
+        )
+      end
+
+      def full_branch_location(arg=nil)
+        set_or_return(
+          :full_branch_location,
+          arg,
+          :kind_of => [String, FalseClass]
+        )
+      end
+
+      def parent(arg=nil)
+        set_or_return(
+          :parent,
+          arg,
+          :kind_of => [String, FalseClass]
+        )
+      end
+
+      def push_location(arg=nil)
+        set_or_return(
+          :push_location,
+          arg,
+          :kind_of => [String, FalseClass]
+        )
+      end
+
+      def stacked_on_location(arg=nil)
+        set_or_return(
+          :stacked_on_location,
+          arg,
+          :kind_of => [String, FalseClass]
+        )
+      end
+
+      def public_location(arg=nil)
+        set_or_return(
+          :public_location,
+          arg,
+          :kind_of => [String, FalseClass]
+        )
+      end
+
+      alias :branch :revision
+      alias :reference :revision
+
+      alias :repo :repository
+
+    end
+  end
+end
